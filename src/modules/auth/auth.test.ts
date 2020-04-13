da0757b2439cebd901882b30d9ae8a944ab3b8b9
jan00newbie <positiveKiddo@gmail.com>
2020-04-13 18:00:37 +0200

    login mutation & tests

--- src/modules/auth/auth.test.ts
+++ src/modules/auth/auth.test.ts
@@ -18,7 +18,7 @@ let token: string;
 test("Registered user should be saved in database.", async () => {
   const response = await request(createUserGQL);
   token = response.data.register;
-  expect(token).not.toBe(null);
+  expect(token).toBeTruthy();
 
   const foundUsers = await User.find({ email: dummyUserData.email });
   expect(foundUsers).toHaveLength(1);
@@ -26,6 +26,10 @@ test("Registered user should be saved in database.", async () => {
   expect(foundUser.username).toBe(dummyUserData.username);
   expect(foundUser.password).not.toBe(dummyUserData.password);
   expect(foundUser.email).toBe(dummyUserData.email);
+
+  // try to register same user
+  const response0 = await request(createUserGQL);
+  expect(response0.data.register).toBe(null);
 });
 
 const meGQL = `
@@ -47,4 +51,21 @@ test("Me query with token should returns userData.", async () => {
   const response1 = await request(meGQL, "bad_token");
   const user1 = response1.data;
   expect(user1).toBe(null);
+});
+
+const loginGQL = (email: string, password: string) => `
+mutation{
+  login(email: "${email}", password: "${password}")
+}`;
+
+test("Login query should return token.", async () => {
+  const goodLoginGQL = loginGQL(dummyUserData.email, dummyUserData.password);
+
+  const response0 = await request(goodLoginGQL);
+  expect(response0.data.login).not.toBe(null);
+
+  const badLoginGQL = loginGQL("Bad email", "Bad passsword");
+
+  const response1 = await request(badLoginGQL);
+  expect(response1.data.login).toBe(null);
 });
--- src/modules/auth/resolvers.ts
+++ src/modules/auth/resolvers.ts
@@ -1,20 +1,19 @@
-import * as bcryptjs from "bcryptjs";
+import * as bcrypt from "bcryptjs";
 
 import { IResolvers } from "graphql-tools";
-// import { AuthenticationError } from "apollo-server-core";
+import { AuthenticationError } from "apollo-server-core";
 import { MutationRegisterArgs } from "../../types/types";
 
 import User from "../../models/user.model";
 
 import { validateToken } from "../../utils/validateToken";
 import { signToken } from "../../utils/signToken";
-import { AuthenticationError } from "apollo-server-core";
 
 export const resolvers: IResolvers = {
   Query: {
     me: async (_, __, context) => {
       const userId = await validateToken(context);
-      console.log(userId);
+
       if (!userId) return new AuthenticationError("Bad token.");
 
       const user = await User.findOne({ _id: userId });
@@ -27,7 +26,13 @@ export const resolvers: IResolvers = {
       _,
       { email, password, username }: MutationRegisterArgs
     ) => {
-      const hashedPassword = await bcryptjs.hash(password, 10);
+      const foundUsers = await User.find({ $or: [{ username }, { email }] });
+
+      if (foundUsers.length > 0) {
+        return new Error("User aready exist!");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       const savedUser = await new User({
         username,
@@ -36,6 +41,21 @@ export const resolvers: IResolvers = {
       }).save();
 
       return signToken(savedUser.id);
+    },
+    login: async (_, { email, password }: MutationRegisterArgs) => {
+      const foundUser = await User.findOne({ email });
+
+      if (!foundUser) return new AuthenticationError("Invalid email or login!");
+
+      const isPasswordCorrect = await bcrypt.compare(
+        password,
+        foundUser.password
+      );
+
+      if (!isPasswordCorrect)
+        return new AuthenticationError("Invalid email or login!");
+
+      return signToken(foundUser.id);
     },
   },
 };
